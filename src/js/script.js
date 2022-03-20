4e95a3612ccb07a50fee3806fc4cfec809098f99
Irullatif <irulsoekamti94@gmail.com>
2022-03-21 01:07:58 +0700

    Update

--- src/js/script.js
+++
@@ -1,9 +0,0 @@
-console.log("halo from index.html");
-
-const initCanvas = (id) => {
-  return new fabric.Canvas(id, {
-    width: 500,
-    height: 500,
-    backgroundColor: "red",
-  });
-};
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   plugins: [require("daisyui")],
   daisyui: {
     styled: true,
-    themes: true,
+    theme: true,
     base: true,
     utils: true,
     logs: true,
